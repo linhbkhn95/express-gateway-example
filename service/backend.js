3276faa7501de92000ae41b814d2b60b06b2b5d6
linhtdb <linhtdb@agiletech.vn>
2019-05-10 18:33:35 +0700

    up

---
+++ service/backend.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const shop = express();
+
+shop
+  .get('/healthz', (req, res, next) => {
+    res.send({ name: 'ghtk', status: 'healthy' });
+    next();
+  })
+  .get('/d/:id', (req, res, next) => {
+    res.send({ discussion: req.params.id });
+    next();
+  })
+  .listen(process.env.PORT || 1337);
+
+const user = express();
+
+user
+  .get('/user', (req, res, next) => {
+    res.send({ user: [ { name: 'linhtd', avatar: 'linhtd' }] });
+    next();
+  })
+  .listen(process.env.PORT2 || 1339)
